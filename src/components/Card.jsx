f9e32fba61f544f5c3ddf667b03a4a38622a98ac
MJOW1999 <michael.jow.williams@gmail.com>
2021-10-27 15:04:31 +0100

    Adding users to a card array

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,38 +7,44 @@ function Card({ name }) {
   
   // console.log('user info', user);
   // console.log('card array', card);
+  const containerArray = [];
+  
 
   React.useEffect(() => {
     fetch(USER_URL + name)
       .then((res) => res.json())
       .then((data) => {
+        setUser(data)
         console.log("set user", setUser(data))
-        return setUser(data)})
-      .then((user) => addCard(user))
-      .then(() => console.log('card array', card));
+        return user})
+      .then((user) => {
+        let temp = [...card];
+        temp.push(user);
+        addCard(temp)
+        console.log("Cards", card)
+        return card})
+      // .then(() => {
+      //   containerArray.push(card)
+      //   console.log("Container array", containerArray)
+      // return containerArray});
   }, [name]);
 
     React.useEffect(() => {
       console.log("Card", card);
+      // return containerArray.push(card);
     }, [card])
 
-  // React.useEffect((user) => {
-  //   addCard(user);
-  // }, [])
 
   
-  // if(user){
-  //   addCard(user);
-  //   console.log('cards', card)
-  // }
 
   if (!user) return <div>Loading...</div>;
   return (
-    <div className="card">
-      <h1>{user.name}</h1>
-      <img src={user.avatar_url} alt="" width="128" height="128" />
-      <button>Click me</button>
-    </div>
+    // <div className="card">
+    //   <h1>{user.name}</h1>
+    //   <img src={user.avatar_url} alt="" width="128" height="128" />
+    //   <button>Click me</button>
+    // </div>
+    <div className="card"></div>
   );
 }
 
