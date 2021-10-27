3ad5292ebfecc8f57f6e0afe1d97f45d70025fd8
“Moggach” <acunnane13@gmail.com>
2021-10-27 17:43:12 +0100

    adding delete button to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,13 @@ const USER_URL = 'https://api.github.com/users/';
 
 function Card({ name }) {
   const [user, setUser] = React.useState();
-  const [cards, addCard] = React.useState([
-    /* {
+  const [cards, addCard, removeCard] = React.useState([
+    {
       avatar_url: 'https://avatars.githubusercontent.com/u/72146072?v=4',
       name: 'Michael Williams',
       repos_url: 'https://api.github.com/users/MJOW1999/repos',
       message: 'blank message',
-    },*/
+    },
   ]);
 
   React.useEffect(() => {
@@ -27,12 +27,20 @@ function Card({ name }) {
       });
   }, [name]);
 
+  function handleRemove(message) {
+    const newList = filteredCards.filter((card) => card.message !== message);
+    addCard(newList);
+  }
+
   const filteredCards = cards.filter((card) => card !== undefined);
-  console.log('filtered array', filteredCards);
+
   const cardList = filteredCards.map((card) => (
     <div>
       <p>{card.message}</p>
       <p>{card.documentation_url}</p>
+      <button type="button" onClick={() => handleRemove(card.message)}>
+        Remove
+      </button>
     </div>
   ));
   return <div>{cardList}</div>;
