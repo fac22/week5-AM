afb0e88e43fe7b52a1c9cd1b2017302d85c12530
“Moggach” <acunnane13@gmail.com>
2021-10-28 14:20:43 +0100

    adding styling to buttons on cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,18 +12,21 @@ function Card({ name }) {
       name: 'Anna Cunnane',
       avatar_url: 'https://avatars.githubusercontent.com/u/43313455?v=4',
       public_repos: 17,
+      html_url: 'https://github.com/Moggach',
     },
     {
       id: 37059,
       name: 'D Sofer',
       avatar_url: 'https://avatars.githubusercontent.com/u/37059?v=4',
       public_repos: 56,
+      html_url: 'https://github.com/sofer',
     },
     {
       id: 72146072,
       name: 'Michael Williams',
       avatar_url: 'https://avatars.githubusercontent.com/u/72146072?v=4',
       public_repos: 18,
+      html_url: 'https://github.com/MJOW1999',
     },
   ]);
 
@@ -55,10 +58,12 @@ function Card({ name }) {
       <h1>{card.name}</h1>
       <img className="avatar" src={card.avatar_url} alt={card.name} />
       <p>Repos: {card.public_repos}</p>
+      <a href={card.html_url}>Profile 🔗</a>
 
       <Counter />
-      <button type="button" onClick={() => handleRemove(card.id)}>
-        Remove card from your collection
+
+      <button className="trash-button" onClick={() => handleRemove(card.id)}>
+        Remove this card 🗑️
       </button>
     </div>
   ));
