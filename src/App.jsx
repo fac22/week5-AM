a63dc268f27b9e900df76969b8f247e469bf36c3
MJOW1999 <michael.jow.williams@gmail.com>
2021-10-28 16:56:37 +0100

    Added desktop styling
    Minor edits to general styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Search from './components/Search';
 function App() {
   return (
     <div className="App">
+      <h4>Which cards will you add to your collection?</h4>
       <Container />
     </div>
   );
